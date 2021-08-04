5999871b91f311dfb43ab6cb006a8403611a9e91
tushar-1 <46473956+tushar-1@users.noreply.github.com>
2021-08-04 12:38:56 +0530

    Initial Commit.

---
+++ src/JavaAssignments/MultipleException.java
@@ -0,0 +1,35 @@
+package JavaAssignments;
+
+public class MultipleException
+    {
+        public static void main(String[] args)
+        {
+            try
+            {
+                int num = 20 / 0;                 //Arithmetic Exception occurs
+                int[] firstArray = {2, 3, 4, 5};
+                int secondNum = firstArray[4];         //ArrayIndexOutOfBoundsExceptionException occurs
+                int numParse = Integer.parseInt("A101");//NullFormatException occurs
+            }
+            catch (ArithmeticException ae)
+            {
+                System.out.println("Division by 0 ");
+            }
+            catch (ArrayIndexOutOfBoundsException aiob)
+            {
+                System.out.println("Wrong array index is refeered");
+            }
+            catch (NullPointerException npe)
+            {
+                System.out.println("Trying to perform operation on Null referrence");
+            }
+            catch (NumberFormatException nfe)
+            {
+                System.out.println("Illegal Parsing being performed");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Exception occured");
+            }
+        }
+    }
---
+++ src/JavaAssignments/MultipleExceptions.java
@@ -0,0 +1,35 @@
+package JavaAssignments;
+
+import java.util.Scanner;
+class MultipleCatchExceptions
+{
+    public static void main(String[] args) {
+        int x, y;
+        Scanner sc = new Scanner(System.in);
+        try
+        {
+            System.out.println("Enter your First Number");
+            x = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter your second number");
+            y = Integer.parseInt(sc.nextLine());
+            System.out.println("z=" + (x/y));
+        }
+        catch (ArithmeticException ae)
+        {
+            System.out.println("A number cannot be divided by 0,illegal operation in Java");
+            System.out.println("Exception Thrown:" + ae);
+            //ArithmeticException will be generated when division by zero
+        }
+        catch(NumberFormatException nfe)
+        {
+            System.out.println("Invalid datatypes are entered,number must be an integer.");
+            System.out.println("Exception Thrown:" + nfe);
+            //NumberFormatException will be generated when enter datatype like float,double
+        }
+        catch(RuntimeException re)
+        {
+            System.out.println("Exception Thrown:" + re);
+        }
+        System.out.println("Out of try catch block");
+    }
+}
